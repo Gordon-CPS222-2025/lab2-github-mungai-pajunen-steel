dedaa7cec300dba17c1259710d1c0858fcb6f115
root <root@BEN-NEWBOOK.>
2025-01-28 17:49:23 -0500

    Replaced own version of tttb_winner_horizontal_test with tttb_winner_draw_test to avoid duplicating team code

--- tictactoe_test.cc
+++ tictactoe_test.cc
@@ -1,7 +1,7 @@
 // Tests for tictactoe.cc
 //
 // Author (starting point): Russ Tuck
-// Authors (everything else: Benjamin Pajunen
+// Authors (everything else): Benjamin Pajunen
 
 #include <iostream>
 using std::cout;
@@ -26,20 +26,23 @@ bool tttb_winner_vertical_test() {
 }
 
 // Added by Benjamin Pajunen
-bool tttb_winner_horizontal_test() {
+bool tttb_winner_draw_test() {
     TictactoeBoard b;
+
+    // Fill the board with a drawn game (neither wins)
     b.set(0, 0, TictactoeXO::O);
     b.set(0, 1, TictactoeXO::O);
-    if (TictactoeXO::O == b.winner()) {
-        cout << "tttb_winner_horizontal_test() failed for 2 in a row" << endl;
-        return false;
-    }
-    b.set(0, 2, TictactoeXO::O);
-    if (TictactoeXO::O != b.winner()) {
-        cout << "tttb_winner_vertical_test() failed for 3 in a row" << endl;
-        return false;
-    }
-    return true;
+    b.set(0, 2, TictactoeXO::X);
+    b.set(1, 0, TictactoeXO::X);
+    b.set(1, 1, TictactoeXO::X);
+    b.set(1, 2, TictactoeXO::O);
+    b.set(2, 0, TictactoeXO::O);
+    b.set(2, 1, TictactoeXO::X);
+    b.set(2, 2, TictactoeXO::O);
+    
+    // Make sure there was no incorrectly judged winner
+    return b.winner() == TictactoeXO::none;
+
 }
 
 // Runs tests above, and prints a message if they all pass.
@@ -47,7 +50,7 @@ bool tttb_winner_horizontal_test() {
 int main() {
     bool result = true;
     result &= tttb_winner_vertical_test();
-    result &= tttb_winner_horizontal_test();        // Added by Benjamin Pajunen
+    result &= tttb_winner_draw_test();        // Added by Benjamin Pajunen
     if (result) {
         cout << "All tests passed." << endl;
         return(0);
