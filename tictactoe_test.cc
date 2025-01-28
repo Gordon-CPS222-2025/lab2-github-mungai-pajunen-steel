127b372174220e90b7c7f86fc2b5ad479284364f
Benjamin Pajunen <benjamin.pajunen@gordon.edu>
2025-01-28 15:34:33 -0500

    Add one test: tttb_winner_horizontal_test()

--- tictactoe_test.cc
+++ tictactoe_test.cc
@@ -1,7 +1,7 @@
 // Tests for tictactoe.cc
 //
 // Author (starting point): Russ Tuck
-// Authors (everything else):
+// Authors (everything else: Benjamin Pajunen
 
 #include <iostream>
 using std::cout;
@@ -25,12 +25,29 @@ bool tttb_winner_vertical_test() {
     return true;
 }
 
+// Added by Benjamin Pajunen
+bool tttb_winner_horizontal_test() {
+    TictactoeBoard b;
+    b.set(0, 0, TictactoeXO::O);
+    b.set(0, 1, TictactoeXO::O);
+    if (TictactoeXO::O == b.winner()) {
+        cout << "tttb_winner_horizontal_test() failed for 2 in a row" << endl;
+        return false;
+    }
+    b.set(0, 2, TictactoeXO::O);
+    if (TictactoeXO::O != b.winner()) {
+        cout << "tttb_winner_vertical_test() failed for 3 in a row" << endl;
+        return false;
+    }
+    return true;
+}
 
 // Runs tests above, and prints a message if they all pass.
 // If any fail, they print their own failure messages.
 int main() {
     bool result = true;
     result &= tttb_winner_vertical_test();
+    result &= tttb_winner_horizontal_test();        // Added by Benjamin Pajunen
     if (result) {
         cout << "All tests passed." << endl;
         return(0);
